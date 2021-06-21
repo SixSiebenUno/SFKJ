36dc2a9e077cc5742ca9d8a21f6b04d696fb233d
SixSiebenUno <luoqiyao671@outlook.com>
2021-06-21 18:02:08 +0800

    check 42

--- psi_analytics_eurocrypt19/join/join.cpp
+++ psi_analytics_eurocrypt19/join/join.cpp
@@ -254,7 +254,6 @@ namespace ENCRYPTO {
 
         context.total_time += (end_time - start_time);
         context.comm_cost += rolecontext.comm_cost;
-        cout << "finished join " <<  (end_time - start_time).count() / CLOCKS_PER_SEC << ' ' << CLOCKS_PER_SEC << endl;
     }
 
     void JoinClient(vector<uint32_t> joinkeyid, vector<vector<uint32_t>> tuples,
